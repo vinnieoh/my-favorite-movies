24115ff7aa0cc20c21d4fca1bf21c3b25caaeadd
Vinnieoh <viniciusoliveira926@gmail.com>
2024-05-20 23:19:11 -0300

    feat: Add config routes

--- api/app/api/api_config.py
+++ api/app/api/api_config.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
 from .v1_endpoints import usuario_route
-
+from .v1_endpoints import comentario_route
+from .v1_endpoints import conteudos_route
 
 api_router = APIRouter()
 
 api_router.include_router(usuario_route.router, prefix='/usuario', tags=['usuario'])
+api_router.include_router(conteudos_route.router, prefix='/conteudos', tags=['conteudos'])
+api_router.include_router(comentario_route.router, prefix='/comentario', tags=['comentario'])
