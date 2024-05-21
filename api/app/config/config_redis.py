91a0d493488a056456fd43150b31005b8058c366
Vinnieoh <viniciusoliveira926@gmail.com>
2024-05-20 23:09:11 -0300

    feat: Finalizacao dos request com redis

--- api/app/config/config_redis.py
+++ api/app/config/config_redis.py
@@ -1,4 +1,5 @@
 from redis import Redis
+import json
 
 from app.config.configs import settings
 
@@ -29,9 +30,31 @@ __redis_connection = RedisConnectionHandle().connect()
 class RedisRepository:
     def __init__(self, redis_conn: Redis) -> None:
         self.__redis_conn = redis_conn
+    
+    def get(self, key: str) -> any:
+        value = self.__redis_conn.get(key)
         
-    def redis_insert(self, key: str, value:any) -> None:
-        self.__redis_conn.set(key, value)
+        if value:
+            return value.decode('utf-8')
+        return None
+
+    def insert_hash(self, key: str, field: str, value: any) -> None:
+        self.__redis_conn.hset(key, field, value)
+
+    def get_hash(self, key: str, field: str) -> any:
+        value = self.__redis_conn.hget(key, field)
+        
+        if value:
+            return value.decode('utf-8')
+        return None
+
+    def insert(self, key: str, value: any, ex: int = settings.REDIS_EXPIRATION_TIME_24H) -> None:
+        value = json.dumps(value)
+        self.__redis_conn.set(key, value, ex=ex)
+
+    def insert_hash_ex(self, key: str, field: str, value: any, ex: int = settings.REDIS_EXPIRATION_TIME_24H) -> None:
+        self.__redis_conn.hset(key, field, value)
+        self.__redis_conn.expire(key, ex)
         
         
 redis_repository = RedisRepository(__redis_connection)
---
+++ api/app/utils/request_api.py
@@ -0,0 +1,130 @@
+import httpx
+from fastapi import HTTPException
+from app.config.configs import settings
+from app.config.config_redis import redis_repository
+
+
+AUTHORIZATION = f"Bearer {settings.API_MOVIE}"
+
+BASE_URL = "https://api.themoviedb.org/3/"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": AUTHORIZATION
+}
+
+
+def request_trading_all_week_br():
+    
+    cache_key = "trending_all_week_br"
+    url = f"{BASE_URL}trending/all/week?language=pt-BR"
+    
+    cached_data = redis_repository.get(cache_key)
+    
+    if cached_data:
+        return cached_data
+    
+    response = httpx.get(url, headers=headers)
+    
+    if response.is_success:
+        data = response.json()
+        redis_repository.insert(cache_key, data)
+        return data
+        
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Erro ao acessar a API")
+    
+
+def request_trading_all_day_br():
+    cache_key = "trending_all_day_br"
+    url = f"{BASE_URL}trending/all/day?language=pt-BR"
+    
+    cached_data = redis_repository.get(cache_key)
+    if cached_data:
+        return cached_data
+    
+    response = httpx.get(url, headers=headers)
+    
+    if response.is_success:
+        data = response.json()
+        redis_repository.insert(cache_key, data)
+        return data
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Erro ao acessar a API")
+    
+
+
+def request_search_conteudo(conteudo: str = 'american dad'):
+    cache_key = f"search_{conteudo}"
+    url = f"{BASE_URL}search/multi?query={conteudo}&include_adult=false&language=pt-BR&page=1"
+    
+    cached_data = redis_repository.get(cache_key)
+    
+    if cached_data:
+        return cached_data
+    
+    response = httpx.get(url, headers=headers)
+    
+    if response.is_success:
+        data = response.json()
+        redis_repository.insert(cache_key, data)
+        return data
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Erro ao acessar a API")
+   
+
+def request_find_by_id(id: int):
+    cache_key = f"movie_{id}"
+    url = f"{BASE_URL}movie/{id}?language=pt-BR"
+    
+    cached_data = redis_repository.get(cache_key)
+    
+    if cached_data:
+        return cached_data
+    
+    response = httpx.get(url, headers=headers)
+    
+    if response.is_success:
+        data = response.json()
+        redis_repository.insert(cache_key, data)
+        return data
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Erro ao acessar a API")
+
+
+def request_movie_id(id: int):
+    cache_key = f"movie_{id}"
+    url = f"{BASE_URL}movie/{id}?language=pt-BR"
+    
+    cached_data = redis_repository.get(cache_key)
+    
+    if cached_data:
+        return cached_data
+    
+    response = httpx.get(url, headers=headers)
+    
+    if response.is_success:
+        data = response.json()
+        redis_repository.insert(cache_key, data)
+        return data
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Erro ao acessar a API")
+
+
+def request_tv_show_id(id: int):
+    cache_key = f"tv_show_{id}"
+    url = f"{BASE_URL}tv/{id}?language=pt-BR"
+    
+    cached_data = redis_repository.get(cache_key)
+    
+    if cached_data:
+        return cached_data
+    
+    response = httpx.get(url, headers=headers)
+    
+    if response.is_success:
+        data = response.json()
+        redis_repository.insert(cache_key, data)
+        return data
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Erro ao acessar a API")
