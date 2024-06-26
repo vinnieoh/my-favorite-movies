3748321aa8a86e21fa7fac24962df136d12e656f
Vinnieoh <viniciusoliveira926@gmail.com>
2024-06-26 09:59:54 -0300

    fix: Correcao rota

--- frontend/src/service/BaseUrlApi.ts
+++ frontend/src/service/BaseUrlApi.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-
 //const url_api = 'http://localhost:8000/api/v1';
-//const url_api = 'http://172.20.0.1/api/v1';
-
-const url_api = ' https://735f-159-89-232-114.ngrok-free.app';
+const url_api = 'http://172.20.0.1/api/v1';
 
 
 const api = axios.create({
