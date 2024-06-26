5350d05f4dae34b5b6e3193439ac56a0a0062ac2
Vinnieoh <viniciusoliveira926@gmail.com>
2024-06-26 00:32:20 -0300

    feat: Add rota api

--- frontend/src/service/BaseUrlApi.ts
+++ frontend/src/service/BaseUrlApi.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 //const url_api = 'http://localhost:8000/api/v1';
 //const url_api = 'http://172.20.0.1/api/v1';
 
-const url_api = 'http://159.89.232.114/api/v1';
+const url_api = ' https://735f-159-89-232-114.ngrok-free.app';
 
 
 const api = axios.create({
